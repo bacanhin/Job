4e61435cb950b2e8c48886d8c540b859cda1e2be
BACANHIN <bacanhin@VIT-P1410>
2017-09-25 16:37:26 -0400

    Se agrego los títulos Estado, Jefe de Estado y Responsable, incluyendo el campo datetime a la vista form en Payroll_Control

--- payroll_control/payroll_control.py
+++ payroll_control/payroll_control.py
@@ -6,11 +6,15 @@ from openerp.osv import fields, osv
 class Gastos_Mensuales(osv.osv):
 	_name = 'gastos.mensuales'
 	_columns = {
-		'partida': fields.char('Partida', required=True),
-		'denominacion': fields.char('Denominacion', required=True),
-		'rubro': fields.char('Rubro', required=True),
-		'cantidad': fields.integer('Cantidad', required=True),
-		'precio': fields.float('Precio', required=True),
+		'_estado': fields.char('Estado', required=True,),
+		'jefe_estado': fields.char('Jefe de Estado', required=True,),
+		'_responsable': fields.char('Responsable', required=True,),
+		'cargar_fecha': fields.datetime('Fecha de carga',),
+		'partida': fields.char('Partida',),
+		'denominacion': fields.char('Denominacion',),
+		'rubro': fields.char('Rubro',),
+		'cantidad': fields.integer('Cantidad',),
+		'precio': fields.float('Precio',),
 		'is_done': fields.boolean('Done?'),
 	}
 
@@ -18,6 +22,6 @@ class Gastos_Nomina(osv.osv):
 	_name = 'gastos.nomina'
 	_columns = {
 		'cargo': fields.selection([('Jubilado','Jubilado'),('Obrero','Obrero'),('Bachiller','Bachiller'),('Bachiller II','Bachiller II'),('Bachiller III','Bachiller III'),('Técnico','Técnico'),('Técnico II','Técnico II'),('Profesional','Profesional'),('Profesional II','Profesional II'),('Profesional III','Profesional III'),('Líder de Área 13%','Líder de Área 13%'),('Líder de Área 15%','Líder de Área 15%'),('Líder de Área 17%','Líder de Área 17%'),('Líder de Área','Líder de Área'),('Coordinador','Coordinador'),('Jefe de Oficina de Estado','Jefe de Oficina de Estado'),('Jefe de Divición','Jefe de Divición'),('Gerente','Gerente'),('Gerente General','Gerente General'),('Presidente','Presidente')],'Cargo', help="Selecciones el cargo que ocupa el trabajador en la Institución"),
-		'cantidad': fields.integer('Cantidad', required=True, help="Indique la cantidad de trabajadores en el mismo cargo"),
-		'monto': fields.float('Monto', required=True),	
+		'cantidad': fields.integer('Cantidad', help="Indique la cantidad de trabajadores en el mismo cargo"),
+		'monto': fields.float('Monto',),	
 	}
